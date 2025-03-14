c1513cbeb42ab0402c91dbdd9f14bea87f828188
Blulu <14637847+lu-huijinii@user.noreply.gitee.com>
2025-03-14 13:10:05 +0800

    提交对 pages.json 的更改

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _imports_0 = "/static/robot-avatar.png";
-const _imports_1 = "/static/logo.png";
-const _imports_2 = "/static/fishing-boat.jpg";
-exports._imports_0 = _imports_0;
-exports._imports_1 = _imports_1;
-exports._imports_2 = _imports_2;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
