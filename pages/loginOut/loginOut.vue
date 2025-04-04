55e7c99d8742e09a8fe7bfe562621e02f05a2bec
Blulu <14637847+lu-huijinii@user.noreply.gitee.com>
2025-04-04 21:10:20 +0800

    进行了优化

---
+++ pages/loginOut/loginOut.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="container">
+    <button class="custom-button" @tap="">退出登录</button>
+  </view>
+</template>
+
+<script  setup>
+import { useUserStore } from `store/modules/user`
+const userStore = useUserStore()
+userStore.token = ''
+console.log(userStore.token);
+
+
+
+
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+  height: 100vh; 
+}
+
+.custom-button {
+  padding: 10px 20px; 
+  font-size: 16px; 
+  color: white; 
+  background-color: #007aff; 
+  border: none; 
+  border-radius: 5px;
+}
+</style>
